de8d09dc43f027671b2f327772b8ceb56691bd39
suzmas <suzanmsucro@gmail.com>
2020-03-15 23:20:48 -0400

    Updates

---
+++ precache-manifest.7f880fc700a4663ec83ea50e61448c59.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ec102eac403e303692e553ecae1516",
+    "url": "/blueprint-test/index.html"
+  },
+  {
+    "revision": "e2a6aa39f3e5fb48f034",
+    "url": "/blueprint-test/static/css/2.345a7bfb.chunk.css"
+  },
+  {
+    "revision": "a4b09e3c4556b8e89280",
+    "url": "/blueprint-test/static/css/main.cfdcbd3f.chunk.css"
+  },
+  {
+    "revision": "e2a6aa39f3e5fb48f034",
+    "url": "/blueprint-test/static/js/2.50829cd7.chunk.js"
+  },
+  {
+    "revision": "23c9bec898e156a6651de32576fd8aa8",
+    "url": "/blueprint-test/static/js/2.50829cd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4b09e3c4556b8e89280",
+    "url": "/blueprint-test/static/js/main.8d79e875.chunk.js"
+  },
+  {
+    "revision": "acb103aa8a2c9c078498",
+    "url": "/blueprint-test/static/js/runtime-main.f53f1086.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/blueprint-test/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/blueprint-test/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/blueprint-test/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/blueprint-test/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/blueprint-test/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/blueprint-test/static/media/icons-20.cef8cdbb.woff"
+  }
+]);
