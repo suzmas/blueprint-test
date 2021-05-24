b6cad1bc3ee0254c84785b41964e66f401a87a9e
suzmas <suzan.sucro@maestroqa.com>
2021-05-23 20:48:17 -0400

    Try to list closed PRs

--- .github/actions/release-bot/index.js
+++ .github/actions/release-bot/index.js
@@ -10,6 +10,22 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
+  const githubToken = core.getInput('repo-token');
+  const octokit = github.getOctokit(githubToken);
+
+  const owner = payload.repository.owner.name;
+  const repo = payload.repository.name;
+  console.log(owner, repo);
+  const closedIssues = octokit.rest.issues.list({
+    owner,
+    repo,
+    state: 'closed',
+    labels: ['merged'],
+    since: '2021-05-23T22:37:46.261Z'
+  });
+
+  console.log(closedIssues);
 } catch (error) {
   core.setFailed(error.message);
 }
