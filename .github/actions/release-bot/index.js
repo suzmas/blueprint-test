68d76960ecdcd0da1891c3628a555920f789648b
suzmas <suzan.sucro@maestroqa.com>
2021-05-23 20:56:21 -0400

    try gain

--- .github/actions/release-bot/index.js
+++ .github/actions/release-bot/index.js
@@ -1,22 +1,9 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-
-  const githubToken = core.getInput('repo-token');
+const thething = async (githubToken, owner, repo) => {
   const octokit = github.getOctokit(githubToken);
 
-  const owner = payload.repository.owner.name;
-  const repo = payload.repository.name;
-  console.log(owner, repo);
   const closedIssues = await octokit.rest.issues.list({
     owner,
     repo,
@@ -26,6 +13,25 @@ try {
   });
 
   console.log(closedIssues);
+}
+try {
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+
+  const githubToken = core.getInput('repo-token');
+  thething(githubToken, owner, repo);
+
+
+  const owner = github.context.payload.repository.owner.name;
+  const repo = github.context.payload.repository.name;
+  console.log(owner, repo);
+  console.log(github.context.payload.repository);
+
 } catch (error) {
   core.setFailed(error.message);
 }
