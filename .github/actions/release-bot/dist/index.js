1c2b7d1376c7c0c1ccbc9d4633017aa82d1eb0a3
suzmas <suzan.sucro@maestroqa.com>
2021-05-23 20:57:53 -0400

    Build action

--- .github/actions/release-bot/dist/index.js
+++ .github/actions/release-bot/dist/index.js
@@ -6190,6 +6190,19 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(219);
 const github = __nccwpck_require__(122);
 
+const thething = async (githubToken, owner, repo) => {
+  const octokit = github.getOctokit(githubToken);
+
+  const closedIssues = await octokit.rest.issues.list({
+    owner,
+    repo,
+    state: 'closed',
+    labels: ['merged'],
+    since: '2021-05-23T22:37:46.261Z'
+  });
+
+  console.log(closedIssues);
+}
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
@@ -6198,7 +6211,16 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+
+  const githubToken = core.getInput('repo-token');
+  thething(githubToken, owner, repo);
+
+
+  const owner = github.context.payload.repository.owner.name;
+  const repo = github.context.payload.repository.name;
+  console.log(owner, repo);
+  console.log(github.context.payload.repository);
+
 } catch (error) {
   core.setFailed(error.message);
 }
