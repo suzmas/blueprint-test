554b083983d549295682d6d03864ae12fc4a48de
suzmas <suzan.sucro@maestroqa.com>
2021-05-25 09:42:56 -0400

    Build action

--- .github/actions/release-bot/dist/index.js
+++ .github/actions/release-bot/dist/index.js
@@ -6190,45 +6190,68 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(219);
 const github = __nccwpck_require__(122);
 
+const todayTagDate = () => {
+  const todayYear = new Date().getFullYear();
+  const todayMonth = new Date().getMonth();
+
+  // tag date should be formatted as YYMM
+  const newTagDate = `${todayYear.toString().slice(2)}${todayMonth.toString().padStart(2, '0')}`;
+  console.log(newTagDate);
+  return newTagDate;
+}
+
+const newTagBuild = (shouldReset, lastTagBuild) => {
+  const lastBuild = shouldReset ? 0 : parseInt(tagChunks[1]) || 0;
+
+  console.log('lastTagBuild', lastBuild);
+  const newTagBuild = (lastBuild + 1).toString().padStart(4, '0');
+  console.log(newTagBuild);
+
+  return newTagBuild;
+}
+
+const makeNewTagName = (lastReleaseTagName) => {
+  const tagChunks = lastReleaseTagName.split('.');
+  console.log(tagChunks);
+    
+  const lastTagDate = tagChunks[0];
+  const lastTagBuild = parseInt(tagChunks[1]);
+
+  const tagDate = todayTagDate();
+  const shouldResetBuild = lastTagDate !== newTagDate;
+  const tagBuild = newTagBuild(shouldResetBuild, lastTagBuild);
+  const newTag = `${tagDate}.${tagBuild}`;
+
+  return newTag;
+}
+
 const thething = async (githubToken, owner, repo) => {
   const octokit = github.getOctokit(githubToken);
 
-  const thing = await octokit.rest.repos.listContributors({ owner, repo });
-  console.log(thing);
   try {
+    const lastRelease = await octokit.rest.repos.getLatestRelease({ owner, repo });
+    console.log(lastRelease);
+
     const closedIssues = await octokit.rest.pulls.list({
       owner,
       repo,
       state: 'closed',
       labels: ['merged'],
-      since: '2021-05-22T22:37:46.261Z'
+      since: lastRelease.data.created_at
     });
-  
+
+    console.log('\n\n/// Closed Issues ///\n\n');
     console.log(closedIssues);
-  } catch (e) {
-    console.log(e);
-  }
 
-  try {
-    const lastRelease = await octokit.rest.repos.getLatestRelease({ owner, repo });
-    console.log(lastRelease);
-    const tagChunks = lastRelease.data.tag_name.split('.');
-    const lastTagDate = tagChunks[0];
-    console.log(tagChunks);
-    const lastTagBuild = parseInt(tagChunks[1]);
-    console.log('lastTagBuild', lastTagBuild);
 
-    const todayDay = new Date().getDay();
-    const todayMonth = new Date().getMonth();
-    const newTagDate = `${todayDay.toString().padStart(2, '0')}${todayMonth.toString().padStart(2, '0')}`;
-    const newTagBuild = (lastTagBuild + 1).toString().padStart(4, '0');
-    const newTag = `${newTagDate}.${newTagBuild}`;
-    console.log('new tag is ', newTag);
-    const makeNewRelease = await octokit.rest.repos.createRelease({ owner, repo, tag_name: newTag });
+    const newTag = makeNewTagName(lastRelease.data.tag_name);
+    
+    await octokit.rest.repos.createRelease({ owner, repo, tag_name: newTag });
   } catch (e) {
     console.log(e);
   }
 }
+
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
