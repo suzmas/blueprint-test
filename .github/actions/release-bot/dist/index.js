ab85711f71bdf1fff4b8c806d5fb54f108b73ed0
suzmas <suzan.sucro@maestroqa.com>
2021-05-30 16:25:02 -0400

    Build release-bot

--- .github/actions/release-bot/dist/index.js
+++ .github/actions/release-bot/dist/index.js
@@ -6195,21 +6195,23 @@ const todayTagDate = () => {
   const todayMonth = new Date().getMonth() + 1; // .getMonth() returns a zero-based value, so we need to inc by 1
 
   // tag date should be formatted as YYMM
-  const newTagDate = `${todayYear.toString().slice(2)}${todayMonth.toString().padStart(2, '0')}`;
+  const newTagDate = `${todayYear.toString().slice(2)}${todayMonth
+    .toString()
+    .padStart(2, "0")}`;
   console.log(newTagDate);
   return newTagDate;
-}
+};
 
 const newTagBuild = (shouldReset, lastTagBuild) => {
   const lastBuild = shouldReset ? 0 : parseInt(lastTagBuild) || 0;
-  const newTagBuild = (lastBuild + 1).toString().padStart(4, '0');
+  const newTagBuild = (lastBuild + 1).toString().padStart(4, "0");
 
   return newTagBuild;
-}
+};
 
 const makeNewTagName = (lastReleaseTagName) => {
-  const tagChunks = lastReleaseTagName.split('.');
-    
+  const tagChunks = lastReleaseTagName.split(".");
+
   const lastTagDate = tagChunks[0];
   const lastTagBuild = parseInt(tagChunks[1]);
 
@@ -6220,49 +6222,79 @@ const makeNewTagName = (lastReleaseTagName) => {
   const newTag = `${tagDate}.${tagBuild}`;
 
   return newTag;
-}
+};
 
-const thething = async (githubToken, owner, repo) => {
-  const octokit = github.getOctokit(githubToken);
+const getLastRelease = async (octokit, owner, repo) => {
+  let lastReleaseData;
   try {
-    const lastRelease = await octokit.rest.repos.getLatestRelease({ owner, repo });
-    const lastReleaseData = lastRelease.data;
+    const lastRelease = await octokit.rest.repos.getLatestRelease({
+      owner,
+      repo,
+    });
+    lastReleaseData = lastRelease.data;
+  } catch (e) {
+    console.log(e);
+  }
+  return lastReleaseData;
+};
 
-    console.log(owner, repo);
-    const prSearchResults = await octokit.rest.search.issuesAndPullRequests({ q: `repo:${owner}/${repo} merged:>${lastReleaseData.created_at} base:master` });
-    console.log(prSearchResults.data.items);
-
-    const mergedPulls = prSearchResults.data.items.map(pullData => `${pullData.title}: ${pullData.html_url}`)
-    console.log(mergedPulls);
-
-    const newTag = makeNewTagName(lastReleaseData.tag_name);
-    console.log('newTag is', newTag);
-    
+const createNewRelease = async (octokit, owner, repo, lastRelease) => {
+  try {
+    const newTag = makeNewTagName(lastRelease.tag_name);
+    console.log("newTag is", newTag);
     await octokit.rest.repos.createRelease({ owner, repo, tag_name: newTag });
   } catch (e) {
     console.log(e);
   }
-}
+};
+
+const getReleasedPRs = async (octokit, owner, repo, lastReleaseTime) => {
+  // TODO -- deploys take a bit -- we can't assume no PRs have been merged since image was built (i.e. we cannot rely on the lastRelease.timeStamp..thisRelase.timeStamp) -- need to get the timestamp for the passed commit SHA -- then filter based on that
+  try {
+    const prSearchResults = await octokit.rest.search.issuesAndPullRequests({
+      q: `repo:${owner}/${repo} merged:>${lastReleaseTime} base:master`,
+    });
+    console.log(prSearchResults.data.items);
+
+    const clubhouseUrlRegex =
+      /(app.clubhouse.io\b\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
+
+    const mergedPulls = prSearchResults.data.items.map((pullData) => {
+      const summary = `${pullData.title}: ${pullData.html_url}`;
+      const clubhouseLinks = pullData.body.match(clubhouseUrlRegex);
+      if (clubhouseLinks.length) {
+        summary += `\nRelated Clubhouse stories: ${clubhouseLinks.join(", ")}`;
+      }
+      return summary;
+    });
+
+    console.log(mergedPulls);
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
+  const time = new Date().toTimeString();
   core.setOutput("time", time);
 
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = github.context.payload;
-  const githubToken = core.getInput('repo-token');
-
+  const { payload } = github.context;
   const owner = payload.repository.owner.login;
   const repo = payload.repository.name;
+
   console.log(owner, repo);
 
-  thething(githubToken, owner, repo);
+  const githubToken = core.getInput("repo-token");
+  const octokit = github.getOctokit(githubToken);
+
+  const lastRelease = getLastRelease(octokit, owner, repo);
+  createNewRelease(octokit, owner, repo, lastRelease);
+  getReleasedPRs(githubToken, owner, repo, lastRelease.createdAt);
 } catch (error) {
   core.setFailed(error.message);
 }
+
 })();
 
 module.exports = __webpack_exports__;
