b7ee09dcfee4e7f5c4ad5be2dd5a0bff174961d8
suzmas <suzan.sucro@maestroqa.com>
2021-05-30 17:38:15 -0400

    Build release-bot

--- .github/actions/release-bot/index.js
+++ .github/actions/release-bot/index.js
@@ -1,41 +1,46 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-const todayTagDate = () => {
+const getNewDateStamp = () => {
   const todayYear = new Date().getFullYear();
-  const todayMonth = new Date().getMonth() + 1; // .getMonth() returns a zero-based value, so we need to inc by 1
+  const todayMonth = new Date().getMonth() + 1; // .getMonth() returns a zero-based value -> inc by 1
 
-  // tag date should be formatted as YYMM
-  const newTagDate = `${todayYear.toString().slice(2)}${todayMonth
-    .toString()
-    .padStart(2, "0")}`;
-  console.log(newTagDate);
-  return newTagDate;
+  const newDateStamp =
+    `${todayYear.toString().slice(2)}` +
+    `${todayMonth.toString().padStart(2, "0")}`;
+
+  return newDateStamp;
 };
 
-const newTagBuild = (shouldReset, lastTagBuild) => {
-  const lastBuild = shouldReset ? 0 : parseInt(lastTagBuild) || 0;
-  const newTagBuild = (lastBuild + 1).toString().padStart(4, "0");
+const getNewBuildNumber = (shouldResetBuild, lastBuildNumber) => {
+  const lastBuildNum = shouldResetBuild ? 0 : parseInt(lastBuildNumber) || 0;
+  const newBuildNum = (lastBuildNum + 1).toString().padStart(4, "0");
 
-  return newTagBuild;
+  return newBuildNum;
 };
 
-const makeNewTagName = (lastReleaseTagName) => {
-  const tagChunks = lastReleaseTagName.split(".");
+/**
+ * Version numbers take the form of: YYMM.xxxx
+ *   a two digit year, a two digit month, followed by a
+ *   zero padded incremented build number, reset as the
+ *   date stamp changes
+ *
+ * Example:
+ *   - Release: March 2021 - 5th build this month
+ *     Version: 2103.0005
+ */
+const getNewVersionNumber = (lastVersionNumber) => {
+  const [lastDateStamp, lastBuildNumber] = lastVersionNumber.split(".");
+  const newDateStamp = getNewDateStamp();
 
-  const lastTagDate = tagChunks[0];
-  const lastTagBuild = parseInt(tagChunks[1]);
+  const shouldResetBuild = lastDateStamp !== newDateStamp;
+  const newBuildNumber = getNewBuildNumber(shouldResetBuild, lastBuildNumber);
 
-  const tagDate = todayTagDate();
-  const shouldResetBuild = lastTagDate !== tagDate;
-  const tagBuild = newTagBuild(shouldResetBuild, lastTagBuild);
-
-  const newTag = `${tagDate}.${tagBuild}`;
-
-  return newTag;
+  const newVersion = `${newDateStamp}.${newBuildNumber}`;
+  return newVersion;
 };
 
-const getLastRelease = async (octokit, owner, repo) => {
+const getLastReleaseData = async (octokit, owner, repo) => {
   let lastReleaseData;
   try {
     const lastRelease = await octokit.rest.repos.getLatestRelease({
@@ -49,28 +54,42 @@ const getLastRelease = async (octokit, owner, repo) => {
   return lastReleaseData;
 };
 
-const createNewRelease = async (octokit, owner, repo, lastRelease) => {
+const createNewRelease = async (
+  octokit,
+  owner,
+  repo,
+  lastRelease,
+  newReleaseDescription
+) => {
   try {
-    const newTag = makeNewTagName(lastRelease.tag_name);
-    console.log("newTag is", newTag);
-    await octokit.rest.repos.createRelease({ owner, repo, tag_name: newTag });
+    const newVersion = getNewVersionNumber(lastRelease.tag_name);
+    console.log("new version is", newVersion);
+    await octokit.rest.repos.createRelease({
+      owner,
+      repo,
+      name: `v${newVersion}`,
+      tag_name: newVersion,
+      body: newReleaseDescription,
+    });
   } catch (e) {
     console.log(e);
   }
 };
 
-const getReleasedPRs = async (octokit, owner, repo, lastReleaseTime) => {
+const getReleasedPRs = async (octokit, owner, repo, lastRelease) => {
   // TODO -- deploys take a bit -- we can't assume no PRs have been merged since image was built (i.e. we cannot rely on the lastRelease.timeStamp..thisRelase.timeStamp) -- need to get the timestamp for the passed commit SHA -- then filter based on that
+  let summaryOfReleasedPRs = "Unknown";
+
   try {
     const prSearchResults = await octokit.rest.search.issuesAndPullRequests({
-      q: `repo:${owner}/${repo} merged:>${lastReleaseTime} base:master`,
+      q: `repo:${owner}/${repo} merged:>${lastRelease.created_at} base:master`,
     });
     console.log(prSearchResults.data.items);
 
     const clubhouseUrlRegex =
       /(app.clubhouse.io\b\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
 
-    const mergedPulls = prSearchResults.data.items.map((pullData) => {
+    const mergedPRList = prSearchResults.data.items.map((pullData) => {
       const summary = `${pullData.title}: ${pullData.html_url}`;
       const clubhouseLinks = pullData.body.match(clubhouseUrlRegex);
       if (clubhouseLinks.length) {
@@ -79,32 +98,39 @@ const getReleasedPRs = async (octokit, owner, repo, lastReleaseTime) => {
       return summary;
     });
 
-    console.log(mergedPulls);
+    summaryOfReleasedPRs = mergedPRList.join("\n\n");
   } catch (e) {
     console.log(e);
   }
-};
 
+  return summaryOfReleasedPRs;
+};
 
 async function run() {
   try {
-    const time = new Date().toTimeString();
-    core.setOutput("time", time);
-  
     // Get the JSON webhook payload for the event that triggered the workflow
     const { payload } = github.context;
     const owner = payload.repository.owner.login;
     const repo = payload.repository.name;
-  
-    console.log(owner, repo);
-  
+
     const githubToken = core.getInput("repo-token");
     const octokit = github.getOctokit(githubToken);
-  
-    const lastRelease = await getLastRelease(octokit, owner, repo);
-    console.log(lastRelease);
-    await createNewRelease(octokit, owner, repo, lastRelease);
-    await getReleasedPRs(octokit, owner, repo, lastRelease.created_at);
+
+    const lastRelease = await getLastReleaseData(octokit, owner, repo);
+    const newReleaseDescription = await getReleasedPRs(
+      octokit,
+      owner,
+      repo,
+      lastRelease
+    );
+
+    await createNewRelease(
+      octokit,
+      owner,
+      repo,
+      lastRelease,
+      newReleaseDescription
+    );
   } catch (error) {
     core.setFailed(error.message);
   }
